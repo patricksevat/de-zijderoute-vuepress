7ae2f33f67cd36ef19c5261aaced18a1fa8b1e5d
Patrick Sevat <p.m.c.sevat@gmail.com>
2020-06-07 14:30:16 +0200

    chore(): very basic way of retrieving Netlify CMS relationships

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,8 @@
 const grayMatter = require('gray-matter');
+const md = require('markdown-it')();
+const glob = require('glob');
 const fs = require('fs');
+const nodePath = require('path');
 
 const replaceRegex = /\/(dynamic-pages|pages)/;
 
@@ -13,23 +16,54 @@ module.exports = {
   description: 'De Zijderoute - Avontuurlijke kinderyoga',
   extendPageData ($page) {
     const {
-      _filePath,           // file's absolute path
-      _computed,           // access the client global computed mixins at build time, e.g _computed.$localePath.
-      _content,            // file's raw content string
-      _strippedContent,    // file's content string without frontmatter
-      key,                 // page's unique hash key
       frontmatter,         // page's frontmatter object
       regularPath,         // current page's default link (follow the file hierarchy)
-      path,                // current page's real link (use regularPath when permalink does not exist)
     } = $page
 
     // Change url to something more friendly
     $page.path = regularPath.replace(replaceRegex, '');
 
-    // 1. Add extra fields.
-    $page.xxx = 'xxx'
-
-    // 2. Change frontmatter.
-    frontmatter.sidebar = 'auto'
+    // TODO better approach:
+    // 0. create plugin
+    // 1. parse Netlify config YML/TOML/JSON/JS
+    // Create a cache for all relation fields
+    // 2. Retrieve list of possible relation fields and the dir location
+    // 3. Hook into the `extendPageData`
+    // 4. set $page[relationFieldName] with frontmatter and (parsed) content
+    if(frontmatter && frontmatter.cards) {
+      const { cards } = frontmatter;
+      $page.cards = cards.map(cardTitle => {
+        const availableCards = retrieveCards();
+        return availableCards.find(card => {
+          return card.frontmatter.title === cardTitle
+        })
+      })
+    }
   }
+}
+
+let cache;
+
+function retrieveCards () {
+  if(cache) {
+    console.log('returning cache');
+    return cache;
+  }
+
+  const cardsDir = nodePath.join(__dirname, '..', 'cards');
+  const allCardsFilePath = glob.sync(`${cardsDir}/*.md`);
+  cache = allCardsFilePath.map(filePath => {
+    const fileContents = fs.readFileSync(filePath, 'utf-8');
+    const { content, data } = grayMatter(fileContents);
+
+    return {
+      _filepath: filePath,
+      _content: fileContents,
+      _strippedContent: content,
+      frontmatter: data,
+      parsedMarkdown: md.render(content),
+    }
+  })
+
+  return cache;
 }
